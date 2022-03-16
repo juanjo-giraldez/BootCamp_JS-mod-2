bd1d0f3efdb0f10c43f49009253ea87074093e9b
Juan Jose Giraldez Jurado <juanjose.giraldez@kairosds.com>
2022-03-17 00:34:56 +0100

    refactor code in file main.js

--- main.js
+++ main.js
@@ -39,9 +39,7 @@ const reseña = confirm("¿Quiere que la reseña se ha anonima?");
 document.getElementById("hotel-name").innerHTML = "Hotel " + hotels[nameHotel].name;
 document.getElementById("hotel-location").innerHTML = "Ubicado en " + hotels[nameHotel].location;
 document.getElementById("rating").innerHTML = "Puntuación  " + quality[rating];
-console.table('first',quality[rating] )
 document.getElementById("reseña").checked = reseña;
 document.getElementById("hotel-img").src = 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/50/d0/d1/hotel-riu-plaza-espana.jpg?w=700&h=300&s=1';
 
-console.log('first',quality[rating] )
 
